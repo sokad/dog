f96da3aea44a642eb3534cc30526ffe545e76a04
Sophiane <sophianekadem@gmail.com>
2025-05-14 08:42:34 +0200

    Second

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func WhenGrowUp(s string) string {
+	return "When the puppy grow up it says: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sokad/dog
+
+go 1.24.2
